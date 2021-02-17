ccca063a7a79ce34081617063647fab1d5512cc1
Rubens Back <rubensback@gmail.com>
2021-02-17 14:10:07 +0000

    finish challenge

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -31,21 +31,70 @@ const CartProvider: React.FC = ({ children }) => {
   useEffect(() => {
     async function loadProducts(): Promise<void> {
       // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const productsList = await AsyncStorage.getItem('Products');
+      if (productsList) setProducts(JSON.parse(productsList));
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const addToCart = useCallback(
+    async product => {
+      // TODO ADD A NEW ITEM TO THE CART
+      const { id, quantity } = product;
+
+      const productsList = products;
+
+      const productAlreadyAddedIndex = products.findIndex(
+        item => item.id === id,
+      );
+
+      if (productAlreadyAddedIndex >= 0) {
+        const productToAdd = productsList[productAlreadyAddedIndex];
+        productsList[productAlreadyAddedIndex] = {
+          ...productToAdd,
+          quantity: productToAdd.quantity + 1,
+        };
+      } else {
+        productsList.push({ ...product, quantity: 1 });
+      }
+
+      await AsyncStorage.setItem('Products', JSON.stringify(productsList));
+      setProducts([...productsList]);
+    },
+    [products],
+  );
 
   const increment = useCallback(async id => {
     // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
+    const productsList = products;
+
+    const productAlreadyAddedIndex = products.findIndex(item => item.id === id);
+
+    const productToAdd = productsList[productAlreadyAddedIndex];
+    productsList[productAlreadyAddedIndex] = {
+      ...productToAdd,
+      quantity: productToAdd.quantity + 1,
+    };
+
+    await AsyncStorage.setItem('Products', JSON.stringify(productsList));
+    setProducts([...productsList]);
   }, []);
 
   const decrement = useCallback(async id => {
     // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
+    const productsList = products;
+
+    const productAlreadyAddedIndex = products.findIndex(item => item.id === id);
+
+    const productToAdd = productsList[productAlreadyAddedIndex];
+    productsList[productAlreadyAddedIndex] = {
+      ...productToAdd,
+      quantity: productToAdd.quantity - 1,
+    };
+
+    await AsyncStorage.setItem('Products', JSON.stringify(productsList));
+    setProducts([...productsList]);
   }, []);
 
   const value = React.useMemo(
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -40,22 +40,31 @@ const Cart: React.FC = () => {
 
   function handleIncrement(id: string): void {
     // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
     // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalPrice = products.reduce((accumulator, current) => {
+      const productTotalPrice = current.price * current.quantity;
+      return productTotalPrice + accumulator;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(totalPrice);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, current) => {
+      return current.quantity + accumulator;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
